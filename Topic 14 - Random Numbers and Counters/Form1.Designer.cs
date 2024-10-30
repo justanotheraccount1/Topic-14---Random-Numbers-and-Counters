c7d959ddc881e6a1e3492bd532e1f54cf8bb8216
Sam Ahrens <339729139@GoTVDSB.ca>
2024-10-30 09:38:04 -0400

    Add project files.

---
+++ Topic 14 - Random Numbers and Counters/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Topic_14___Random_Numbers_and_Counters
+{
+    partial class FormRandomCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRandomize = new System.Windows.Forms.Button();
+            this.lblRandomNumber = new System.Windows.Forms.Label();
+            this.lblRandomCounter = new System.Windows.Forms.Label();
+            this.lblLargestNum = new System.Windows.Forms.Label();
+            this.lblSmallestNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRandomize
+            // 
+            this.btnRandomize.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRandomize.Location = new System.Drawing.Point(33, 120);
+            this.btnRandomize.Name = "btnRandomize";
+            this.btnRandomize.Size = new System.Drawing.Size(415, 52);
+            this.btnRandomize.TabIndex = 0;
+            this.btnRandomize.Text = "Generate Random Number";
+            this.btnRandomize.UseVisualStyleBackColor = true;
+            this.btnRandomize.Click += new System.EventHandler(this.btnRandomize_Click);
+            // 
+            // lblRandomNumber
+            // 
+            this.lblRandomNumber.AutoSize = true;
+            this.lblRandomNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRandomNumber.Location = new System.Drawing.Point(488, 120);
+            this.lblRandomNumber.Name = "lblRandomNumber";
+            this.lblRandomNumber.Size = new System.Drawing.Size(40, 42);
+            this.lblRandomNumber.TabIndex = 1;
+            this.lblRandomNumber.Text = "0";
+            // 
+            // lblRandomCounter
+            // 
+            this.lblRandomCounter.AutoSize = true;
+            this.lblRandomCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRandomCounter.Location = new System.Drawing.Point(54, 203);
+            this.lblRandomCounter.Name = "lblRandomCounter";
+            this.lblRandomCounter.Size = new System.Drawing.Size(394, 37);
+            this.lblRandomCounter.TabIndex = 2;
+            this.lblRandomCounter.Text = "0 Random Numbers made";
+            // 
+            // lblLargestNum
+            // 
+            this.lblLargestNum.AutoSize = true;
+            this.lblLargestNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLargestNum.Location = new System.Drawing.Point(61, 269);
+            this.lblLargestNum.Name = "lblLargestNum";
+            this.lblLargestNum.Size = new System.Drawing.Size(131, 20);
+            this.lblLargestNum.TabIndex = 3;
+            this.lblLargestNum.Text = "Largest Number: ";
+            // 
+            // lblSmallestNum
+            // 
+            this.lblSmallestNum.AutoSize = true;
+            this.lblSmallestNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSmallestNum.Location = new System.Drawing.Point(61, 306);
+            this.lblSmallestNum.Name = "lblSmallestNum";
+            this.lblSmallestNum.Size = new System.Drawing.Size(134, 20);
+            this.lblSmallestNum.TabIndex = 4;
+            this.lblSmallestNum.Text = "Smallest Number:";
+            // 
+            // FormRandomCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSmallestNum);
+            this.Controls.Add(this.lblLargestNum);
+            this.Controls.Add(this.lblRandomCounter);
+            this.Controls.Add(this.lblRandomNumber);
+            this.Controls.Add(this.btnRandomize);
+            this.Name = "FormRandomCounter";
+            this.Text = "Random Numbers and Counters";
+            this.Load += new System.EventHandler(this.FormRandomCounter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRandomize;
+        private System.Windows.Forms.Label lblRandomNumber;
+        private System.Windows.Forms.Label lblRandomCounter;
+        private System.Windows.Forms.Label lblLargestNum;
+        private System.Windows.Forms.Label lblSmallestNum;
+    }
+}
+
---
+++ Topic 14 - Random Numbers and Counters/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_14___Random_Numbers_and_Counters
+{
+    public partial class FormRandomCounter : Form
+    {   
+        Random generator = new Random();
+        int counter, randNum, numSmallest, numLargest;
+        public FormRandomCounter()
+        {
+            InitializeComponent();
+        }
+
+        private void FormRandomCounter_Load(object sender, EventArgs e)
+        {
+            counter = 0;
+            numSmallest = 1000;
+            numLargest = 0;
+        }
+
+        private void btnRandomize_Click(object sender, EventArgs e)
+        {
+            randNum = generator.Next(1001);
+            counter++;
+            lblRandomCounter.Text = counter + " Random Numbers made";
+            lblRandomNumber.Text = randNum.ToString();
+            if (randNum <= numSmallest)
+            {
+                numSmallest = randNum;
+                lblSmallestNum.Text = "Samllest Number: " + numSmallest;
+            }
+            if (randNum >= numLargest)
+            {
+                numLargest = randNum;
+                lblLargestNum.Text = "Largest Number: " + numLargest;
+            }
+        }
+    }
+}
